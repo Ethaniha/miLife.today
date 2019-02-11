7b54dbbe640f4a601885c791e6662f33d2cbb293
stiffers <35315214+stiffers@users.noreply.github.com>
2019-02-11 22:04:40 +0000

    register page added

--- index.php
+++ index.php
@@ -1,25 +1,60 @@
+<?php 
+session_start();
+?>
+
 <html>
    
   <head>
     <title>Login Page</title>
       
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
       
   </head>
    
   <body bgcolor = "#FFFFFF">
     
-  <nav class="navbar navbar-light bg-light">
-    <form class="form-inline">
-      <a class="navbar-brand" href="#">
-        <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-      </a>
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  <nav class="navbar navbar-expand-sm bg-light navbar-light">
+
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" alt="Logo">
+    </a>
+
+    <form class="form-inline mx-auto  my-auto" action="/action_page.php">
+            <div class="input-group">
+                <input class="form-control py-2 border-right-0 border" type="Search for something.." value="search" id="example-search-input">
+                <span class="input-group-append">
+                    <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                </span>
+            </div>
     </form>
+
+
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <i class="fas fa-bell fa-2x" data-count="2" style="color:grey"></i>
+      </li>
+      <li class="nav-item">
+        <i class="fas fa-cogs fa-2x" style="color:grey"></i>
+      </li>
+      <li class="nav-item">
+
+        <?php if(isset($_SESSION['login_user'])) {
+          echo '<a href="user.php"><i class="fas fa-user fa-2x" style="color:grey"></i></a>';
+        } else {
+          echo '<a href="login.php"><i class="fas fa-user fa-2x" style="color:grey"></i></a>';
+        } ?>  
+      </li>
+    </ul>
   </nav>
+
 
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
   </body>
 </html>
+
+
+
+
--- register.php
+++ register.php
@@ -6,34 +6,41 @@
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
    session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT User_ID FROM users WHERE email = '$myusername' and password = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-        
-      if($count == 1) {
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: homepage.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
+  
+    if(isset($_POST["submit"])) {
+
+        $forename = $_POST['forename'];
+        $surname = $_POST['surname'];
+        $email= $_POST['email'];
+        $pass = $_POST['password'];
+
+        $forename = mysqli_real_escape_string($db,$forename);
+        $surname = mysqli_real_escape_string($db,$surname);
+        $email = mysqli_real_escape_string($db,$email);
+
+        $sql="SELECT email FROM users WHERE email='$email'";
+        $result=mysqli_query($db,$sql);
+        $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+        if(mysqli_num_rows($result) == 1)
+        {
+          $msg = "Sorry...This email already exist...";
+        }
+        else
+        {
+          $query = mysqli_query($db, "INSERT INTO users (forename, surname, email, password)VALUES ('$forename', '$surname', '$email', '$pass')");
+          if($query)
+          {
+            $msg = "Thank You! you are now registered.";
+          }
+        }
       }
-   }
+
 ?>
 <html>
    
    <head>
-      <title>Login Page</title>
+      <title>Register Page</title>
       
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
       <link rel="stylesheet" href="styles.css">
@@ -73,30 +80,42 @@
     </nav>
     
     <div class="container">
-      <h2>Log-in</h2>
-      <form class="form-horizontal" action="" method="post">
-        <div class="form-group">
-          <label class="control-label col-sm-2" name="username">Email:</label>
-          <div class="col-sm-10">
-            <input type="email" class="form-control" name="username" placeholder="Enter email" name="email">
+      <h2>Register</h2>
+      <form action="" method="post">
+
+        <div class="form-row">
+
+          <div class="form-group col-md-3">
+            <label for="inputEmail4">Forename:</label>
+            <input type="text" class="form-control" id="forename" placeholder="forename" name="forename">
           </div>
-        </div>
-        <div class="form-group">
-          <label class="control-label col-sm-2" name="password">Password:</label>
-          <div class="col-sm-10">          
-            <input type="password" class="form-control" name="password" placeholder="Enter password" name="pwd">
+
+          <div class="form-group col-md-3">
+            <label for="inputEmail4">Forename:</label>
+            <input type="text" class="form-control" id="surname" placeholder="surname" name="surname">
           </div>
+
         </div>
+
+        <div class="form-group">
+          <label class="control-label" name="username">Email:</label>
+          <input type="email" class="form-control" placeholder="Enter email" name="email">
+        </div>
+
+        <div class="form-group">
+          <label class="control-label" name="password">Password:</label>     
+            <input type="password" class="form-control" name="password" placeholder="Enter password">
+        </div>
+
         <div class="form-group">        
-          <div class="col-sm-offset-2 col-sm-10">
             <div class="checkbox">
               <label><input type="checkbox" name="remember"> Remember me</label>
             </div>
-          </div>
         </div>
+
         <div class="form-group">        
-          <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default">Submit</button>
+          <div class="col-sm-offset-2">
+            <button type="submit" class="btn btn-primary" name="submit">Submit</button>
           </div>
         </div>
         Or click <a href="register.php">here</a> to register
