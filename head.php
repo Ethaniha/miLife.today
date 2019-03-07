d77bfaf1bbfe7015ec71edfbb37fec30c89c4745
will <13015928+willwoodruff@users.noreply.github.com>
2019-03-07 00:12:19 +0000

    Realigned Search bar to center of screen

--- head.php
+++ head.php
@@ -3,34 +3,36 @@
   <nav class="navbar navbar-expand-sm bg-light navbar-light">
 
     <div class="container">
-   <a href="index.php" ><img src="assets/logo.png" alt="Logo" class="navbar-logo"></a>
-
+      <div class="navbar-leftside">
+   <a href="index.php" ><img src="assets/logo.png" alt="Logo" id="navbar-logo"></a>
+  </div>
 <?php if(isset($_SESSION['login_user'])) {
-    echo'<form class="form-inline mx-auto  my-auto" action="/action_page.php">
+    echo'<div class="navbar-center">
+    <form class="d-inline w-100 form-inline mx-auto my-auto" action="/action_page.php">
             <div class="input-group">
-                <input class="form-control py-2 border-right-0 border" type="Search for something.." placeholder="Search for something.." id="example-search-input">
+                <input class="form-control py-2 border" type="Search for something.." placeholder="Search for something.." id="search-input">
                 <span class="input-group-append">
                     <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
                 </span>
             </div>
-    </form>'; } ?>
+    </form></div>'; } ?>
 
 
-    <ul class="navbar-nav">
+    <ul class="navbar-nav navbar-rightside">
 <?php if(isset($_SESSION['login_user'])) {
       echo '<li class="nav-item">
-        <i class="fas fa-bell fa-2x" data-count="2" style="color:grey"></i>
+        <i class="fas fa-bell fa-2x navbar-icon" data-count="2"  style="color: grey"></i>
       </li>
       <li class="nav-item">
-        <a href="user_settings.php"><i class="fas fa-cogs fa-2x" style="color:grey"></i></a>
+        <a href="user_settings.php"><i class="fas fa-cogs fa-2x navbar-icon" style="color: grey"></i></a>
       </li>';
     } ?>
       <li class="nav-item">
 
         <?php if(isset($_SESSION['login_user'])) {
-          echo '<a href="user_profile.php?username='.$_SESSION['username'].'"><i class="fas fa-user fa-2x" style="color:grey"></i></a>';
+          echo '<a href="user_profile.php?username='.$_SESSION['username'].'"><i class="fas fa-user fa-2x navbar-icon" style="color: grey" ></i></a>';
         } else {
-          echo '<a href="login.php"><i class="fas fa-user fa-2x" style="color:grey"></i></a>';
+          echo '<a href="login.php"><i class="fas fa-user fa-2x navbar-icon" style="color: grey"></i></a>';
         } ?>  
       </li>
     </ul>
