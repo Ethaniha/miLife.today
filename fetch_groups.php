1c4cf6c33bcba5e2da7af75635ccc4a45278c945
stiffers <35315214+stiffers@users.noreply.github.com>
2019-03-27 00:53:46 +0000

    hyperlink to the group profile

--- fetch_groups.php
+++ fetch_groups.php
@@ -11,21 +11,21 @@
 	$searchOuput = "";
  
 
-  	$sql = "SELECT groups.name, groups.description, groups.image, groups.owner FROM groups, group_users WHERE groups.id = group_users.group_id AND group_users.user_id = '$user_id'";
+  	$sql = "SELECT groups.name, groups.description, groups.image, groups.owner, groups.id FROM groups, group_users WHERE groups.id = group_users.group_id AND group_users.user_id = '$user_id'";
 	$result = mysqli_query($db, $sql) or die(mysqli_error($db));
 
   	if(mysqli_num_rows($result) > 0) {
 
   		while ($row = mysqli_fetch_array($result)) {
 
-  			$searchOuput .= "<li class='list-group-item' id='user' style='background-color:#f1eeee;'>
+  			$searchOuput .= "<a href='group_profile.php?group_id=".$row[4]."'><li class='list-group-item' id='user' style='background-color:#f1eeee;'>
   								<div class='col-xs-3'>
               						<img src='../Assets/imgs/groups/".$row[2]."' class='profilePhoto' width=50 height=50/>
               						<span style='font-size:16px;'><strong>".$row[0]."</strong></span>
               						 - 
               						<span style='font-size:16px;'>".$row[1]."</span>
             					</div>
-  							</li>";
+  							</li></a>";
 
   		}
 
