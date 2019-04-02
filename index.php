2e770a0b9546185a36d0e07c97edf762c84f7475
will <13015928+willwoodruff@users.noreply.github.com>
2019-04-02 17:52:49 +0100

    Fixes to like Notifictaions

--- index.php
+++ index.php
@@ -6,16 +6,20 @@ include("db.php");
   function notify($body, $id, $type, $db) {
 
     if ($type == 2) {
-      $sql = "SELECT post.user_id FROM post, post_likes WHERE post.post = post_likes.post_id";
-      $result = mysqli_query($db, $sql);
-      $row=mysqli_fetch_array($result);
-      $receiverID = $row[0];
 
       $myusername = $_SESSION['login_user'];
       $sql = "SELECT User_ID FROM users WHERE email = '$myusername' ";
       $result = mysqli_query($db, $sql);
       $row=mysqli_fetch_array($result);
       $user_id = $row[0];
+
+
+      $sql = "SELECT post.user_id FROM post
+      JOIN post_likes ON post.post = post_likes.post_id WHERE post_likes.id = '$id' AND post.user_id != '$user_id'";
+      $result = mysqli_query($db, $sql);
+      $row = mysqli_fetch_array($result);
+      $receiverID = $row[0];
+
 
       $sql = "INSERT INTO notifications (type, sender_id, receiver_id, post_id) VALUES (2, '$user_id', '$receiverID', '$id')";
       $result = mysqli_query($db, $sql) or die(mysqli_error($db));
@@ -91,9 +95,9 @@ if (isset($_GET['postid'])) {
     if (mysqli_num_rows($result) < 1) {
       $sql = "UPDATE post SET likes = likes + 1 WHERE post = $postid";
       $result = mysqli_query($db, $sql) or die(mysqli_error($db));
-      $sql = "INSERT INTO post_likes (post_id, user_id) VALUES ($postid, $user_id)";
-      $result = mysqli_query($db, $sql) or die(mysqli_error($db));
 
+      $sql = "INSERT INTO post_likes (post_id, user_id) VALUES ('$postid', '$user_id' )";
+      $result = mysqli_query($db, $sql) or die(mysqli_error($db));
       $last_row = mysqli_insert_id($db);
       notify("", $last_row, 2, $db);
 
